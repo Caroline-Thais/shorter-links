b442661ef6c246ad5ac70adf81c9b2f992e86896
Thaís Caroline Silva <thaissilva@MacBook-Air-de-Thais.local>
2022-06-16 21:14:13 -0300

    first commit

---
+++ shorterlink/src/components/Home.js
@@ -0,0 +1,16 @@
+import { memo } from 'react'
+
+
+const Home = () => (
+    <div>
+        <div>
+            <h1>Mais do que um encurtador de links</h1>
+            <p>
+                Contrua a reputacao da sua marca e receba detalhes
+                sobre as performances dos seus links.
+            </p>
+        </div>
+    </div>
+)
+
+export default memo(Home);
